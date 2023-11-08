20874c3712577457e92aa113220cbbc60f57e247
henriquemilioli <72499498+henriquemilioli@users.noreply.github.com>
2023-11-08 09:27:34 -0300

    Add project files.

---
+++ MusicStreaming.Application/Conta/Dto/CriarUsuarioDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStreaming.Application.Conta.Dto
+{
+    public class CriarUsuarioDTO
+    {
+        public String Nome {  get; set; }
+
+        public String CPF {  get; set; } 
+    }
+}
---
+++ MusicStreaming.Core/Exception/BusinessException.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MusicStreaming.Core.Exception
+{
+    internal class BusinessException
+    {
+    }
+}
